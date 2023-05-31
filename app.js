74ab42fd958ce06d165a851d3d7a7b4e26f10848
joacoalbarracin <joacoalbarracin@gmail.com>
2023-05-31 20:41:58 -0300

    Arreglo app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static("public"))
 
-app.listen(process.env.PORT ||3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo")
 })
 
