cebb9aa76de49537f668c1a1f0d51adfc8c29676
joacoalbarracin <joacoalbarracin@gmail.com>
2023-05-31 19:34:56 -0300

    Subiendo Mercado Liebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(process.env.PORT ||3000, () => {
+    console.log("Servidor corriendo")
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
